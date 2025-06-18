6f422e0455ac51663f5feabb61eb9f9a0ff0dcd5
ronaldcumbal <ronald.cumbal.g@gmail.com>
2025-06-18 19:59:50 +0200

    updated launch file with vdeo and microphone from Furhat robot nodes

--- ros2_workspace/src/woz_reception/launch/woz_launch.py
+++ ros2_workspace/src/woz_reception/launch/woz_launch.py
@@ -25,7 +25,29 @@ def generate_launch_description():
             ]
     )   
 
+    camera_furhat_node = Node(
+            package='woz_reception',
+            namespace='woz_reception',
+            executable='camera_furhat',
+            name='camera_furhat',
+            parameters=[
+                {'camera_ip': '10.0.1.10:3000'}
+            ]
+    )
+
+    microphone_furhat_node = Node(
+            package='woz_reception',
+            namespace='woz_reception',
+            executable='microphone_furhat',
+            name='microphone_furhat',
+            parameters=[
+                {'camera_ip': '10.0.1.10:3001'}
+            ]
+    )
+
     ld.add_action(wizard_interface_node)
     ld.add_action(furnat_bridge_node)
+    ld.add_action(camera_furhat_node)
+    ld.add_action(microphone_furhat_node)
     return ld   
  
